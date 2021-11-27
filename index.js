f921480c9c171066a2a420fd591f262fa5f1ece5
dsvivass <dsvivass@unal.edu.co>
2021-11-27 15:50:20 -0500

    Script ready to be connected to the backend

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ inputElements.forEach(element => {
 
 })
 
-// FUNCION PARA CALCULAR COORDENADAS DE LOS ELEMENTOS
+// FUNCIONES
 
 const calculateElementCoordinates = () => {
 
@@ -86,7 +86,7 @@ const calculateElementCoordinates = () => {
     // Elemento 6
     // [[coord final elemento 4], [a + b, 0]
     coordsObject['6'] = [coordsObject['4'][1],
-        [parseFloat(a.value) + parseFloat(b.value), 0]
+    [parseFloat(a.value) + parseFloat(b.value), 0]
     ];
 
     // Elemento 7
@@ -97,59 +97,311 @@ const calculateElementCoordinates = () => {
 
 }
 
-const drawElementLines = (canvas, coords, h, factor) => {
+const drawElementLines = (context, coords, h_init, factor, forces) => {
 
-    const context = canvas.getContext('2d');
-
-    context.scale(0.7, 0.7)
-
-    // Altura vertical de partida
-    var h = parseFloat(h.value) * factor * 1.1
+    colors = {'C': '#00FF00', 'T': '#a80a0a'}
 
     Object.keys(coords).forEach(key => {
 
         context.beginPath();
-        context.moveTo(coords[key][0][0] * factor, h - coords[key][0][1] * factor);
-        context.lineTo(coords[key][1][0] * factor, h - coords[key][1][1] * factor);
-        context.strokeStyle = '#00ff00';
+        context.moveTo(coords[key][0][0] * factor, h_init - coords[key][0][1] * factor);
+        context.lineTo(coords[key][1][0] * factor, h_init - coords[key][1][1] * factor);
+        context.strokeStyle = colors[forces[key]['condition']];
         context.stroke();
 
     })
 
 }
 
-const drawTextForces = (canvas, coords, h, factor) => {
+const drawTextForces = (context, coords, h_init, factor, forces) => {
 
-    const context = canvas.getContext('2d');
-
-    // Altura vertical de partida
-    var h = parseFloat(h.value) * factor * 1.1
-
-    context.font = "8px Arial";
+    context.font = "12px Arial";
 
     Object.keys(coords).forEach(key => {
 
-            context.fillText(key,
-
-                (coords[key][0][0] * factor + coords[key][1][0] * factor) / 2,
-                h - (coords[key][0][1] * factor + coords[key][1][1] * factor) / 2)
-        })
+        context.fillText(
+            forces[key]['value'],
+            (coords[key][0][0] * factor + coords[key][1][0] * factor) / 2,
+            h_init - (coords[key][0][1] * factor + coords[key][1][1] * factor) / 2
+        )
+    })
 }
 
-document.getElementById('btnSubmit').onclick = function () {
+const drawSupports = (context, h_init, coord_x) => {
+
+    // Factor de escala, respecto h_init
+    const supportFactor = 1 / 10        // 1 / 10 de la altura de inicio
+
+    // Dibujo de triangulo (Soporte articulado)
+    context.fillStyle = '#03d3fc'
+    context.beginPath();
+    context.moveTo(0, h_init);
+    context.lineTo(h_init * supportFactor, h_init + h_init * supportFactor);
+    context.lineTo(- h_init * supportFactor, h_init + h_init * supportFactor);
+    context.closePath();
+    context.fill();
+
+    // Dibujo de circulo (Rodillo)
+    context.beginPath();
+    context.arc(coord_x, h_init + h_init * supportFactor / 2, h_init * supportFactor / 2, 0, Math.PI * 2, true);
+    context.fill();
+
+    return supportFactor // Para reutilizarlo al ubicar las flechas de reacción
+}
+
+const drawReactionAndForces = (context, h_init, supportFactor, coords, factor, requestObject, reactions) => {
+
+    // Factor de escala, respecto h_init
+    const reactionTailFactor = 1 / 5       // 1 / 5 de la altura de inicio
+
+    context.strokeStyle = '#52545c'
+
+    // Reaccion 1 en y
+
+    context.beginPath();
+    context.moveTo(0, h_init + h_init * supportFactor);
+    context.lineTo(h_init * supportFactor / 2, (h_init + h_init * supportFactor) + h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(0, h_init + h_init * supportFactor);
+    context.lineTo(0, (h_init + h_init * supportFactor) + h_init * reactionTailFactor);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(0, h_init + h_init * supportFactor);
+    context.lineTo(- h_init * supportFactor / 2, (h_init + h_init * supportFactor) + h_init * supportFactor / 2);
+    context.stroke();
+
+    // Reaccion 1 en x
+
+    context.beginPath();
+    context.moveTo(0, h_init);
+    context.lineTo(coords['2'][1][0] * factor * 2 / 5 , h_init);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['2'][1][0] * factor * 2 / 5, h_init);
+    context.lineTo(coords['2'][1][0] * factor * 2 / 5 - h_init * supportFactor / 2, h_init - h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['2'][1][0] * factor * 2 / 5, h_init);
+    context.lineTo(coords['2'][1][0] * factor * 2 / 5 - h_init * supportFactor / 2, h_init + h_init * supportFactor / 2);
+    context.stroke();
+
+    // Reaccion elemento 2 en y
+
+    context.beginPath();
+    context.moveTo(coords['7'][1][0] * factor, h_init + h_init * supportFactor);
+    context.lineTo(coords['7'][1][0] * factor + h_init * supportFactor / 2, (h_init + h_init * supportFactor) + h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['7'][1][0] * factor, h_init + h_init * supportFactor);
+    context.lineTo(coords['7'][1][0] * factor, (h_init + h_init * supportFactor) + h_init * reactionTailFactor);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['7'][1][0] * factor, h_init + h_init * supportFactor);
+    context.lineTo(coords['7'][1][0] * factor - h_init * supportFactor / 2, (h_init + h_init * supportFactor) + h_init * supportFactor / 2);
+    context.stroke();
+
+    // Reaccion 2 en x
+
+    context.beginPath();
+    context.moveTo(coords['7'][1][0] * factor, h_init);
+    context.lineTo(coords['7'][1][0] * factor * 4 / 5 , h_init);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['7'][1][0] * factor, h_init);
+    context.lineTo(coords['7'][1][0] * factor - h_init * supportFactor / 2, h_init - h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['7'][1][0] * factor, h_init);
+    context.lineTo(coords['7'][1][0] * factor - h_init * supportFactor / 2, h_init + h_init * supportFactor / 2);
+    context.stroke();
+
+    // Fuerza p1
+    context.beginPath();
+    context.moveTo(coords['1'][1][0] * factor, h_init - coords['1'][1][1] * factor / 2);
+    context.lineTo(coords['1'][1][0] * factor + h_init * supportFactor / 2, (h_init - coords['1'][1][1] * factor / 2) - h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['1'][1][0] * factor, h_init - coords['1'][1][1] * factor / 2);
+    context.lineTo(coords['1'][1][0] * factor - h_init * supportFactor / 2, (h_init - coords['1'][1][1] * factor / 2) - h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['1'][1][0] * factor, h_init - coords['1'][1][1] * factor / 2);
+    context.lineTo(coords['1'][1][0] * factor, h_init - coords['1'][1][1] * factor);
+    context.stroke();
+
+    // Fuerza p2
+    context.beginPath();
+    context.moveTo(coords['1'][1][0] * factor, h_init - coords['1'][1][1] * factor);
+    context.lineTo(coords['1'][1][0] * factor - h_init * supportFactor / 2, (h_init - coords['1'][1][1] * factor) - h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['1'][1][0] * factor, h_init - coords['1'][1][1] * factor);
+    context.lineTo(coords['1'][1][0] * factor - h_init * supportFactor / 2, (h_init - coords['1'][1][1] * factor) + h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['1'][1][0] * factor, h_init - coords['1'][1][1] * factor);
+    context.lineTo(coords['1'][1][0] * factor - coords['1'][1][0] * factor, h_init - coords['1'][1][1] * factor);
+    context.stroke();
+
+    // Fuerza p3
+    context.beginPath();
+    context.moveTo(coords['4'][1][0] * factor, h_init - coords['4'][1][1] * factor / 2);
+    context.lineTo(coords['4'][1][0] * factor + h_init * supportFactor / 2, (h_init - coords['4'][1][1] * factor / 2) - h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['4'][1][0] * factor, h_init - coords['4'][1][1] * factor / 2);
+    context.lineTo(coords['4'][1][0] * factor - h_init * supportFactor / 2, (h_init - coords['4'][1][1] * factor / 2) - h_init * supportFactor / 2);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(coords['4'][1][0] * factor, h_init - coords['4'][1][1] * factor / 2);
+    context.lineTo(coords['4'][1][0] * factor, h_init - coords['4'][1][1] * factor);
+    context.stroke();
+
+    // Texto de fuerzas y reacciones
+
+    context.fillStyle = '#0a27a8'
+    context.font = "15px Arial"
+
+    // Reaccion1y
+    context.fillText(
+        reactions['1']['y'],
+        h_init * supportFactor / 2,
+        (h_init + h_init * supportFactor) + h_init * reactionTailFactor
+
+    )
+
+    // Reaccion1x
+    context.fillText(
+        reactions['1']['x'],
+        coords['2'][1][0] * factor * 2 / 5 - h_init * supportFactor / 2,
+        h_init + h_init * supportFactor * 1.5
+
+    )
+
+    // Reaccion2y
+    context.fillText(
+        reactions['2']['y'],
+        coords['7'][1][0] * factor - h_init * supportFactor * 2,
+        (h_init + h_init * supportFactor) + h_init * reactionTailFactor
+
+    )
+
+    // Reaccion2x
+    context.fillText(
+        reactions['2']['x'],
+        coords['7'][1][0] * factor * 4 / 5 ,
+        h_init + h_init * supportFactor * 1.5
+
+    )
+
+    // Fuerza1
+    context.fillText(
+        requestObject['loads']['p1'],
+        coords['1'][1][0] * factor + h_init * supportFactor / 2,
+        (h_init - coords['1'][1][1] * factor / 2) - h_init * supportFactor
+
+    )
+
+    // Fuerza2
+    context.fillText(
+        requestObject['loads']['p2'],
+        coords['1'][1][0] * factor - coords['1'][1][0] * factor,
+        (h_init - coords['1'][1][1] * factor) + h_init * supportFactor
+
+    )
+
+    // Fuerza3
+    context.fillText(
+        requestObject['loads']['p3'],
+        coords['4'][1][0] * factor + h_init * supportFactor / 2,
+        (h_init - coords['4'][1][1] * factor / 2) - h_init * supportFactor
+
+    )
+
+}
+
+document.getElementById('btnSubmit').onclick = async function () {
 
     const coords = calculateElementCoordinates()
     const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    // const url = 'localhost:3000'
+
+    // const request = await fetch('url')
+    // const response = await request.json()
+
+    const response = {
+        "forces": {
+            "1": {
+                "value": 111,
+                "condition": "T"
+            },
+            "2": {
+                "value": 222,
+                "condition": "C"
+            },
+            "3": {
+                "value": 333,
+                "condition": "T"
+            },
+            "4": {
+                "value": 444,
+                "condition": "T"
+            },
+            "5": {
+                "value": 555,
+                "condition": "C"
+            },
+            "6": {
+                "value": 666,
+                "condition": "T"
+            },
+            "7": {
+                "value": 777,
+                "condition": "T"
+            }
+        },
+
+        "reactions": {
+            "1": {
+                "x": 123,
+                "y": 321
+            },
+            "2": {
+                "x": 789,
+                "y": 987
+            }
+        }
+        
+    }
+
+    context.scale(0.7, 0.7)
 
     // Factor de escala, para que a + b => 300px
     const factor = parseFloat(canvas.width) / (parseFloat(a.value) + parseFloat(b.value))
 
-    // h se utiliza para manejar el eje y negativo
-    drawElementLines(canvas, coords, h, factor)
+    // Altura vertical de partida, se utiliza para manejar el eje y negativo
+    var h_init = parseFloat(h.value) * factor * 1.1
 
-    drawTextForces(canvas, coords, h, factor)
-
-
-
+    drawElementLines(context, coords, h_init, factor, response['forces'])
+    drawTextForces(context, coords, h_init, factor, response['forces'])
+    supportFactor = drawSupports(context, h_init, coords['7'][1][0] * factor)
+    drawReactionAndForces(context, h_init, supportFactor, coords, factor, requestObject, response['reactions'])
 
 }
